23bcfc202cc78ecd5ffe19889a691857870b72d9
Josh Wedekind <josh@halfnibble.com>
2024-02-03 13:58:49 -0800

    Add model and data routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Bread = require('../models/bread');
 
 // Breads Routes
 router.get('/', (req, res) => {
-    res.send('This is the index at /breads');
+    res.send(Bread);
+});
+
+router.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex]);
 });
 
 module.exports = router;
