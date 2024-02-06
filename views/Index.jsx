4f2ccf98b4cc055896f0b299aba5f1b9f75368b1
Josh Wedekind <josh@halfnibble.com>
2024-02-05 18:24:08 -0800

    Add links to bread details

--- views/Index.jsx
+++ views/Index.jsx
@@ -6,8 +6,10 @@ const Index = ({ breads }) => {
         <Default>
             <h2>Index page</h2>
             <ul>
-                {breads.map((bread) => (
-                    <li key={bread.name}>{bread.name}</li>
+                {breads.map((bread, index) => (
+                    <li key={bread.name}>
+                        <a href={`/breads/${index}`}>{bread.name}</a>
+                    </li>
                 ))}
             </ul>
         </Default>
