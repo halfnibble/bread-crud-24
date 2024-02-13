e8feffd9a312ef4a397543185f2dea2917dd31a9
Josh Wedekind <josh@halfnibble.com>
2024-02-12 19:57:30 -0800

    Add mongoose and configure Bread model.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const mongoose = require('mongoose');
 const app = express();
 
 // CONFIG
 require('dotenv').config();
 const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Database Connection
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log('connected to mongo: ' + MONGO_URI);
+    })
+    .catch((err) => {
+        console.log('Error connecting to mongo: ' + err);
+    });
 
 // MIDDLEWARE
 app.use(express.static('public'));
